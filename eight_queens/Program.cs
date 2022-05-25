b2ded6e99cde2dbbe64ed3aadba870e900cb631d
Анастасія Шевцова <nastya.sova2004@gmail.com>
2022-05-25 16:42:15 +0300

    add class Quenns

--- eight_queens/Program.cs
+++ eight_queens/Program.cs
@@ -10,6 +10,8 @@ namespace eight_queens
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
---
+++ eight_queens/Queens.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eight_queens
+{
+    class Queens
+    {
+        private readonly int numQueens = 8;
+
+        //print solution
+        public void PrintBoard(int[,] board)
+        {
+            for (int i = 0; i < numQueens; i++)
+            {
+                for (int j = 0; j < numQueens; j++)
+                {
+                    Console.WriteLine(" " + board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+
+    }
+}
